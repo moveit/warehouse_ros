4db618a718de0f41e59ca02720e4e8649f1801a1
Ioan Sucan <isucan@willowgarage.com>
2012-11-05 14:07:34 +0200

    one more attempt at adding symbols to this lib

--- src/mongo_ros_dummy.cpp
+++ src/mongo_ros_dummy.cpp
@@ -33,13 +33,15 @@
 // add this dummy function so the .so file copies everything we need from the
 // libmongoclient.a file at link time. We need this because Ubuntu does not install
 // a .so file for libmongoclient and the wrappers we have in this lib are templated.
-static void thisFunctionShouldNeverBeCalled(void)
+// make this function globally accessible so strip --strip-unneeded does not remove symbols
+void thisFunctionShouldNeverBeCalled(void)
 {
   ros::NodeHandle nh;
   boost::shared_ptr<mongo::DBClientConnection> conn = mongo_ros::makeDbConnection(nh, "", 0, 0.0f);
   mongo::GridFS *gfs = new mongo::GridFS(*conn, "");
   mongo::BSONObj q;
   mongo::GridFile f = gfs->findFile(q);
+  f.write(std::cout);
   gfs->removeFile("");
   q = gfs->storeFile(NULL, 0, "");
   mongo::LT;
