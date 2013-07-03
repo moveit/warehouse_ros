b0197529d50cc76612afb7d637a93cdef716d314
isucan <isucan@gmail.com>
2013-07-03 17:19:26 +0300

    add config.h.in for deciding how to include mongo headers

--- cmake/FindMongoDB.cmake
+++ cmake/FindMongoDB.cmake
@@ -1,16 +1,20 @@
-# - Find MongoDB
+# - Find MongoDB; NOTE: this is specific to warehouse_ros!
+#
 # Find the MongoDB includes and client library
 # This module defines
 #  MongoDB_INCLUDE_DIR, where to find mongo/client/dbclient.h
 #  MongoDB_LIBRARIES, the libraries needed to use MongoDB.
 #  MongoDB_FOUND, If false, do not try to use MongoDB.
+#  MongoDB_EXPOSE_MACROS, If true, mongo_ros should use '#define MONGO_EXPOSE_MACROS'
 
 if(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
    set(MongoDB_FOUND TRUE)
 
 else(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
 
-  find_path(MongoDB_INCLUDE_DIR mongo/client/dbclient.h
+  set(MongoDB_EXPOSE_MACROS "NO")
+
+  set(MongoDB_PossibleIncludePaths
       /usr/include/
       /usr/local/include/
       /usr/include/mongo/
@@ -19,6 +23,16 @@ else(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
       $ENV{ProgramFiles}/Mongo/*/include
       $ENV{SystemDrive}/Mongo/*/include
       )
+  find_path(MongoDB_INCLUDE_DIR mongo/client/dbclientinterface.h
+    ${MongoDB_PossibleIncludePaths})
+
+  if(MongoDB_INCLUDE_DIR)
+    find_path(MongoDB_dbclientinterface_Path mongo/client/dbclientinterface.h
+      ${MongoDB_PossibleIncludePaths})
+    if (MongoDB_dbclientinterface_Path)
+      set(MongoDB_EXPOSE_MACROS "YES")
+    endif()
+  endif()
 
 if(WIN32)
   find_library(MongoDB_LIBRARIES NAMES mongoclient
@@ -45,6 +59,7 @@ endif(WIN32)
   if(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
     set(MongoDB_FOUND TRUE)
     message(STATUS "Found MongoDB: ${MongoDB_INCLUDE_DIR}, ${MongoDB_LIBRARIES}")
+    message(STATUS "MongoDB using new interface: ${MongoDB_EXPOSE_MACROS}")
   else(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
     set(MongoDB_FOUND FALSE)
     if (MongoDB_FIND_REQUIRED)
@@ -54,6 +69,6 @@ endif(WIN32)
 	endif (MongoDB_FIND_REQUIRED)
   endif(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
 
-  mark_as_advanced(MongoDB_INCLUDE_DIR MongoDB_LIBRARIES)
+  mark_as_advanced(MongoDB_INCLUDE_DIR MongoDB_LIBRARIES MongoDB_EXPOSE_MACROS)
 
 endif(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
