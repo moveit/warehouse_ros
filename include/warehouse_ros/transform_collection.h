ccb83db6c9e329633e63a3c66e6179e635a7ea1f
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2021-09-14 04:39:48 -0700

    Updated tf2_geometry_msgs.h to tf2_geometry_msgs.hpp  (#85)

--- include/warehouse_ros/transform_collection.h
+++ include/warehouse_ros/transform_collection.h
@@ -41,7 +41,12 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_msgs/msg/tf_message.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+// TODO(v4hn): remove after EOL galactic
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
 #include <tf2_ros/buffer.h>
 
 namespace warehouse_ros
